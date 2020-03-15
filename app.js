7fc56d3505ca21840280a0a2052b8b904a047d27
Jason <23126557+dyckia@users.noreply.github.com>
2020-03-14 22:17:15 -0230

    Add index page

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const express = require('express'),
       path = require('path'),
       bodyParser = require('body-parser'),
-      pg = require('pg'),
+      {Client} = require('pg'),
       app = express();
-
-const connectionString = 'postgresql://Ostu:ottoman@localhost:5432/recipebookdb';
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const client = new Client({
+        user: 'Ostu',
+        host: 'localhost',
+        database: 'recipebookdb',
+        password: '12345',
+        port: 5432,
+    });
+    client.connect();
+    client.query('SELECT * FROM recipes', (err, result) => {
+        res.render('index', {recipes: result.rows});
+        client.end();
+    });
+});
+
+app.post('/add', (req, res) => {
+    const client = new Client({
+        user: 'Ostu',
+        host: 'localhost',
+        database: 'recipebookdb',
+        password: '12345',
+        port: 5432,
+    });
+    client.connect();
+    client.query('INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)',
+                 [req.body.name, req.body.ingredients, req.body.directions],
+                 (err, result) => {
+        res.redirect('/');
+        client.end();
+    });
 });
 
 app.listen(3000, () => {
